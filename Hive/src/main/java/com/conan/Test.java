caa2492ca1313f369e83c08be4c063132b15ce25
liufeiqiang <liu.feiqiang@puscene.com>
2018-07-31 19:55:31 +0800

    update

---
+++ Hive/src/main/java/com/conan/Test.java
@@ -0,0 +1,10 @@
+package com.conan;
+
+/**
+ * Created by Administrator on 2018/7/20.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
