bc2b2cf2c3137b15a139c0217f66c8f92ec589c9
liufeiqiang <liu.feiqiang@puscene.com>
2018-08-01 18:26:29 +0800

    pom更新

--- Hive/src/main/java/com/conan/bigdata/hive/fileformat/OrcReader.java
+++ Hive/src/main/java/com/conan/bigdata/hive/fileformat/OrcReader.java
@@ -18,6 +18,12 @@ public class OrcReader {
     public static void main(String[] args) throws IOException {
         String path = "hdfs://ns1/user/hive/warehouse/ods.db/resta_citytable";
         Configuration conf = new Configuration();
+        conf.set("dfs.nameservices", "ns1");
+        conf.set("dfs.ha.namenodes.ns1", "nn1,nn2");
+        conf.set("dfs.namenode.rpc-address.ns1.nn1", "m01.dpbu.mwee.prd:8020");
+        conf.set("dfs.namenode.rpc-address.ns1.nn2", "m02.dpbu.mwee.prd:8020");
+        conf.set("dfs.client.failover.proxy.provider.ns1", "org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider");
+        conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
         Path filePath = new Path(path);
 
         FileSystem fs = FileSystem.get(conf);
@@ -26,12 +32,12 @@ public class OrcReader {
         StructObjectInspector inspector = (StructObjectInspector) reader.getObjectInspector();
         RecordReader records = reader.rows();
 
-        Object row=null;
+        Object row = null;
 
         System.out.println("下面开始输出内容：");
-        while (records.hasNext()){
-            row=records.next(row);
-            List<Object> value_list=inspector.getStructFieldsDataAsList(row);
+        while (records.hasNext()) {
+            row = records.next(row);
+            List<Object> value_list = inspector.getStructFieldsDataAsList(row);
             System.out.println(value_list);
         }
     }
