e5bf942d65901717371ea41ece6032085c321cb2
liufeiqiang <liu.feiqiang@puscene.com>
2018-08-10 11:46:44 +0800

    update

--- Common/src/main/java/test/AAA.java
+++ Common/src/main/java/test/AAA.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/4/25.
  */
@@ -8,13 +10,7 @@ public class AAA {
     public static int[] a = new int[1 + 10 / 32];
 
     public static void main(String[] args) throws InterruptedException {
-        String[] s=new String[]{"a","b","c","d","e","f"};
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < s.length; i += 2) {
-            sb.append("\"").append(s[i]).append("\":\"").append(s[i + 1]).append("\"").append(",");
-        }
-        System.out.println(sb.length());
-        sb.deleteCharAt(sb.length()-1);
-        System.out.println(sb);
+        List<String> list=null;
+        System.out.println(list.toString());
     }
 }
--- Hive/src/main/java/com/conan/bigdata/hive/udf/GenerateJson.java
+++ Hive/src/main/java/com/conan/bigdata/hive/udf/GenerateJson.java
@@ -3,10 +3,7 @@ package com.conan.bigdata.hive.udf;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
-import org.apache.hadoop.hive.serde2.objectinspector.MapObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
-import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
 import org.apache.hadoop.io.Text;
@@ -18,13 +15,16 @@ import java.util.Arrays;
  */
 public class GenerateJson extends GenericUDF {
 
-    private MapObjectInspector mapObjectInspector = null;
-
     //这个方法只调用一次，并且在evaluate()方法之前调用。该方法接受的参数是一个ObjectInspectors数组。该方法检查接受正确的参数类型和参数个数。
     @Override
     public ObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
         if (objectInspectors.length % 2 != 0 || objectInspectors.length == 0) {
             throw new UDFArgumentException("The number of parameters must be even number, Every two parameters make up K-V from begin to end !");
+        }
+
+        for (int i = 0; i < objectInspectors.length; i += 2) {
+            if (!(objectInspectors[i] instanceof StringObjectInspector) || !ObjectInspector.Category.PRIMITIVE.equals(objectInspectors[i + 1].getCategory()))
+                throw new UDFArgumentException("The K of parameters must be string type, The V of parameters must be primitive type!");
         }
 
 //        ObjectInspector returnOI = PrimitiveObjectInspectorFactory.getPrimitiveJavaObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.STRING);
@@ -35,8 +35,10 @@ public class GenerateJson extends GenericUDF {
     //这个方法类似UDF的evaluate()方法。它处理真实的参数，并返回最终结果。接受任意参数的个数
     @Override
     public Object evaluate(DeferredObject[] deferredObjects) throws HiveException {
-
-        Text[] argument = (Text[]) deferredObjects;
+        String[] argument = new String[deferredObjects.length];
+        for (int i = 0; i < deferredObjects.length; i++) {
+            argument[i] = nvl(deferredObjects[i].get());
+        }
 
         return getJson(argument);
     }
@@ -47,12 +49,19 @@ public class GenerateJson extends GenericUDF {
         return Arrays.toString(strings);
     }
 
-    private Text getJson(Text[] s) {
+    private Text getJson(String[] s) {
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < s.length; i += 2) {
             sb.append("\"").append(s[i]).append("\":\"").append(s[i + 1]).append("\"").append(",");
         }
         sb.deleteCharAt(sb.length() - 1);
         return new Text("{" + sb.toString() + "}");
+    }
+
+    private String nvl(Object s) {
+        if (s == null)
+            return "";
+        else
+            return s.toString().trim();
     }
 }
