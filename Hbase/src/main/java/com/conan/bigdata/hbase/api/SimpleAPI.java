59902a920944bb503c96d181cff0c62fe97631ac
liufeiqiang <liu.feiqiang@puscene.com>
2018-09-06 16:07:15 +0800

    update

--- Hbase/src/main/java/com/conan/bigdata/hbase/api/SimpleAPI.java
+++ Hbase/src/main/java/com/conan/bigdata/hbase/api/SimpleAPI.java
@@ -1,5 +1,7 @@
 package com.conan.bigdata.hbase.api;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.conan.bigdata.hbase.common.CONSTANT;
 import com.conan.bigdata.hbase.util.HBaseUtils;
 import org.apache.commons.io.FileUtils;
@@ -11,6 +13,10 @@ import org.apache.hadoop.hbase.util.Bytes;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2018/9/3.
@@ -41,6 +47,8 @@ public class SimpleAPI {
         scan.setCaching(1000);
         scan.setCacheBlocks(true);
 
+        Map<String, String> map = new HashMap<>();
+        List<String> jsonList = new ArrayList<>();
         File f = new File(OUT_PATH);
         ResultScanner rs = null;
         try {
@@ -48,9 +56,32 @@ public class SimpleAPI {
             rs = table.getScanner(scan);
             for (Result r : rs) {
                 for (Cell cell : r.listCells()) {
-                    String value = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength()) + "\n";
-//                    System.out.print(value);
-                    FileUtils.write(f, value, Charset.defaultCharset(), true);
+                    String value = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+//                    System.out.println(value);
+                    String[] values = value.split("\\|");
+                    if (!"点菜".equals(values[3]))
+                        break;
+                    JSONObject json = JSON.parseObject(values[17]);
+                    map.put("id", values[1]);
+                    map.put("mw_id", values[2]);
+                    map.put("action", values[3]);
+                    map.put("state_id", values[4]);
+                    map.put("state_name", values[5]);
+                    map.put("business_time", values[6]);
+                    map.put("brand_id", values[7]);
+                    map.put("brand_name", values[8]);
+                    map.put("shop_id", values[9]);
+                    map.put("shop_name", values[10]);
+                    map.put("category_name", values[11]);
+                    map.put("city_id", values[12]);
+                    map.put("city_name", values[13]);
+                    map.put("province_id", values[14]);
+                    map.put("province_name", values[15]);
+                    map.put("bc_name", values[16]);
+                    json.putAll(map);
+//                    jsonList.add(json.toJSONString());
+                    map.clear();
+                    FileUtils.write(f, json.toJSONString() + "\n", Charset.defaultCharset(), true);
                 }
             }
         } catch (IOException e) {
@@ -58,10 +89,30 @@ public class SimpleAPI {
         } finally {
             rs.close();
         }
+
+
+//        try {
+//            Table table = HBaseUtils.getConnection().getTable(tableName);
+//            rs = table.getScanner(scan);
+//            for (Result r : rs) {
+//                for (Cell cell : r.listCells()) {
+//                    String value = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+//                    System.out.println(value);
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            rs.close();
+//        }
     }
 
+
     public static void main(String[] args) throws IOException {
-//        getResult(TableName.valueOf(CONSTANT.TABLE_NAME), "162175342");
+        long start = System.currentTimeMillis();
+//        getResultScan(TableName.valueOf("user_tag_detail4"), "2480909613242");
         getResultScan(TableName.valueOf(CONSTANT.TABLE_NAME), new StringBuilder("162175342").reverse().toString());
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
     }
 }
--- Hive/src/main/java/com/conan/bigdata/hive/jdbc/Hive2.java
+++ Hive/src/main/java/com/conan/bigdata/hive/jdbc/Hive2.java
@@ -19,10 +19,61 @@ public class Hive2 {
     }
 
     public static void main(String[] args) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("select default.dining_destination_tags('1,4,5')");
-        ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-            System.out.println(rs.getString(1));
-        }
+        PreparedStatement ps = conn.prepareStatement("set mapreduce.job.name = user_tag_detail");
+        System.out.println(ps.getClass().getName());
+        ps.addBatch("set hive.exec.dynamic.partition.mode = nonstrict");
+        ps.addBatch("set mapreduce.job.name = user_tag_detail");
+        ps.addBatch("insert overwrite table dmd.user_tag_detail_new partition (action_id)\n" +
+                "select\n" +
+                "    /*+ mapjoin(s1) */\n" +
+                "    concat(a.mw_id, unix_timestamp(a.business_time, 'yyyy-MM-dd HH:mm:ss'), a.action_id, ceil(rand()*100000)) as row_key,\n" +
+                "    a.id,\n" +
+                "    a.mw_id,\n" +
+                "    a.action,\n" +
+                "    a.state_id,\n" +
+                "    a.state_name,\n" +
+                "    a.business_time,\n" +
+                "    nvl(s2.shopid, '') as brand_id,\n" +
+                "    nvl(s2.shopname, '') as brand_name,\n" +
+                "    nvl(s1.shopid, '') as shopid,\n" +
+                "    nvl(s1.shopname, '') as shop_name,\n" +
+                "    nvl(s1.categoryname, '') as category_name,\n" +
+                "    nvl(s1.cityid, '') as cityid,\n" +
+                "    nvl(s1.cityname, '') as cityname,\n" +
+                "    nvl(s1.provinceid, '') as provinceid,\n" +
+                "    nvl(s1.provincename, '') as provincename,\n" +
+                "    nvl(s1.bcname, '') as bcname,\n" +
+                "    a.action_attr,\n" +
+                "    a.action_id\n" +
+                "from\n" +
+                "(\n" +
+                "    select\n" +
+                "        a.id,\n" +
+                "        coalesce(b.mw_id, d.mw_id, c.mw_id, '') as mw_id,\n" +
+                "        a.action_id,\n" +
+                "        a.action,\n" +
+                "        a.state_id,\n" +
+                "        a.state_name,\n" +
+                "        a.business_time,\n" +
+                "        a.brand_id,\n" +
+                "        a.shop_id,\n" +
+                "        a.action_attr\n" +
+                "    from dmd.user_tag_detail_new_test a\n" +
+                "    left join dmd.user_info_temp b\n" +
+                "        on (case when a.user_id = '' then concat('', rand()) else a.user_id end) = b.accountname and b.flag = '1'-- and b.account_type = 'userid'\n" +
+                "    left join dmd.user_info_temp c\n" +
+                "        on (case when a.mobile = '' then concat('', rand()) else a.mobile end) = c.accountname and c.flag = '2'\n" +
+                "    left join dmd.user_info_temp d\n" +
+                "        on (case when a.open_id = '' then concat('', rand()) else a.open_id end) = d.accountname and d.flag = '3'\n" +
+                "    where coalesce(b.mw_id, d.mw_id, c.mw_id, '') <> ''\n" +
+                ") a\n" +
+                "left join dmd.shoptable_test s1\n" +
+                "    on (case when a.shop_id = 0 then concat('', rand()) else a.shop_id end) = s1.shopid\n" +
+                "left join dmd.shoptable_test s2\n" +
+                "    on a.brand_id = s2.shopid\n" +
+                "where s1.shopid is not null or s2.shopid is not null");
+        ps.executeBatch();
+        conn.commit();
+
     }
 }
