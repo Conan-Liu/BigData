07141d14f9b57e713795165243d0e9544b0376ab
liufeiqiang <liufeiqiang@aspirecn.com>
2019-06-05 22:03:20 +0800

    update

--- Flume/src/main/java/com/conan/bigdata/flume/interceptor/FilterWordInterceptor.java
+++ Flume/src/main/java/com/conan/bigdata/flume/interceptor/FilterWordInterceptor.java
@@ -6,6 +6,7 @@ import org.apache.flume.interceptor.Interceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -17,13 +18,24 @@ public class FilterWordInterceptor implements Interceptor {
 
     private static final Logger LOG = LoggerFactory.getLogger(FilterWordInterceptor.class);
 
+    public FilterWordInterceptor() {
+        LOG.warn("这是构造的方法...................................");
+    }
+
     @Override
     public void initialize() {
         LOG.warn("这是初始化的方法...................................");
     }
 
+    /**
+     * 如果不想要的Event 返回null就好
+     *
+     * @param event
+     * @return
+     */
     @Override
     public Event intercept(Event event) {
+        LOG.warn("这是intercept 单个Event的方法...................................");
         Map<String, String> headers = event.getHeaders();
         String body = new String(event.getBody());
         try {
@@ -39,32 +51,44 @@ public class FilterWordInterceptor implements Interceptor {
             LOG.error("格式转换错误: [" + body + "]");
             return null;
         }
+        LOG.warn("suffix**************" + event.getHeaders().get("suffix"));
         return event;
     }
 
     @Override
     public List<Event> intercept(List<Event> events) {
+        LOG.warn("这是intercept 多个Event的方法...................................");
+        List<Event> list = new ArrayList<>(events.size());
         for (Event event : events) {
-            intercept(event);
+//            intercept(event);
+            Event e1 = intercept(event);
+            // 如果为null，就不输出了， 起到过滤作用
+            if (e1 != null) {
+                list.add(e1);
+            }
+            LOG.warn("**************" + e1.getHeaders().get("suffix"));
         }
-        return events;
+        LOG.warn("===============" + list.size());
+        return list;
     }
 
     @Override
     public void close() {
-
+        LOG.warn("这是close的方法...................................");
     }
 
     public static class Builder implements Interceptor.Builder {
 
         @Override
         public Interceptor build() {
+            LOG.warn("这是build的方法...................................");
             return new FilterWordInterceptor();
         }
 
         @Override
         public void configure(Context context) {
-
+            LOG.warn("configure...................................");
+            LOG.info("param ==== " + context.getString("param"));
         }
     }
 }
