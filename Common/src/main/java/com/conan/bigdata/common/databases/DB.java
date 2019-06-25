b085ee7bd8a36359a53a7dc239055ae9be8c8a8b
liufeiqiang <liu.feiqiang@puscene.com>
2019-06-25 14:09:57 +0800

    update

--- Common/src/main/java/com/conan/bigdata/common/databases/DB.java
+++ Common/src/main/java/com/conan/bigdata/common/databases/DB.java
@@ -4,7 +4,7 @@ import java.util.Properties;
 
 
 public enum DB {
-    TEST_MYSQL_19_6("10.0.19.6", "business_olap","pd_test_dev","VFR5rgdf"),
+    TEST_MYSQL_19_6("10.0.19.6", "business_olap", "pd_test_dev", "VFR5rgdf"),
     MYSQL_24_5_TRANSFORM("10.0.24.5", "transform", "bigdata", "u0j@>uRFsjJi0uExv[yZ"),
     MYSQL_24_6_TRANSFORM("10.0.24.6", "transform", "bigdata", "u0j@>uRFsjJi0uExv[yZ"),
     MYSQL_24_7_TRANSFORM("10.0.24.7", "transform", "bigdata", "u0j@>uRFsjJi0uExv[yZ"),
@@ -24,14 +24,15 @@ public enum DB {
     MYSQL_32_RESTAURANT("10.1.25.60", "restaurant", "pd_hadoop_dev", "4_hadoop_D32F"),
     MYSQL_249_RESTAURANT("10.0.146.37", "restaurant", "pd_hadoop_dev", "4_hadoop_D32F"),
     MYSQL_80_ASSOCIATOR("10.0.146.80", "associator", "bigdata", "v(Yrbh2ekwBDEIu(]ue3"),
-    MYSQL_50_ASSOCIATOR("10.1.32.185", "associator","rdpususertag_dev",  "dcff73ec05d73a4d5ae0ea47a0030541"), //阿里云迁移，从10.0.32.5 改为 10.1.32.150 by Sui 20170313
-    MYSQL_181_ASSOCIATOR("10.1.32.181", "associator","devuser2",  "LC2q@dM=T5!pmoAwf5k"), //阿里云迁移，从10.0.32.5 改为 10.1.32.150 by Sui 20170313
-    MYSQL_100_ASSOCIATOR("10.0.146.33", "associator","devuser2",  "LC2q@dM=T5!pmoAwf5k"),
-    MYSQL_66_ASSOCIATOR("10.0.21.66", "associator","associator",  "LC2q@dM=T5!pmoAwf5k"),  //测试
-    MYSQL_24_ASSOCIATOR("10.0.24.24", "estimate","devuser2",  "LC2q@dM=T5!pmoAwf5k"),  //预估模型用
-    MYSQL_33_ASSOCIATOR("10.0.146.33", "associator","associator",  "LC2q@dM=T5!pmoAwf5k"), //测试  13307
-    ORACLE_166_BWSWD("10.0.21.166","ORCL", "pmadw",  "pmadev021"),
-    MYSQL_13_PLAT("10.1.36.13", "plat_db","pd_hadoop_dev",  "Flh5atLxtw4kak"),
+    MYSQL_50_ASSOCIATOR("10.1.32.185", "associator", "rdpususertag_dev", "dcff73ec05d73a4d5ae0ea47a0030541"), //阿里云迁移，从10.0.32.5 改为 10.1.32.150 by Sui 20170313
+    MYSQL_181_ASSOCIATOR("10.1.32.181", "associator", "devuser2", "LC2q@dM=T5!pmoAwf5k"), //阿里云迁移，从10.0.32.5 改为 10.1.32.150 by Sui 20170313
+    MYSQL_100_ASSOCIATOR("10.0.146.33", "associator", "devuser2", "LC2q@dM=T5!pmoAwf5k"),
+    MYSQL_66_ASSOCIATOR("10.0.21.66", "associator", "associator", "LC2q@dM=T5!pmoAwf5k"),  //测试
+    MYSQL_24_ASSOCIATOR("10.0.24.24", "estimate", "devuser2", "LC2q@dM=T5!pmoAwf5k"),  //预估模型用
+    MYSQL_33_ASSOCIATOR("10.0.146.33", "associator", "associator", "LC2q@dM=T5!pmoAwf5k"), //测试  13307
+    MYSQL_18_81_COUPONS("10.0.18.81", "coupons", "devuser2", "LC2q@dM=T5!pmoAwf5k"), // 会员测试库
+    ORACLE_166_BWSWD("10.0.21.166", "ORCL", "pmadw", "pmadev021"),
+    MYSQL_13_PLAT("10.1.36.13", "plat_db", "pd_hadoop_dev", "Flh5atLxtw4kak"),
     ORACLE_37_BWSWD("10.0.24.10", "BQSWD", "pmadw", "pmadev021"),
     ORACLE_7_BWSWD("10.0.24.7", "BQSWD", "pmadw", "pmadev021"),
     ORACLE_9_BWSWD("10.0.24.9", "BQSWD", "pmadw", "pmadev120"),
@@ -59,7 +60,7 @@ public enum DB {
             this.url = String.format("jdbc:mysql://%s:3306/%s?%s", ip, database, this.MYSQL_PARAMETERS);
             this.driver = "com.mysql.jdbc.Driver";
             this.properties.put("driver", this.driver);
-        } else if(this.toString().startsWith("TEST")){
+        } else if (this.toString().startsWith("TEST")) {
             this.url = String.format("jdbc:mysql://%s:30242/%s?%s", ip, database, this.MYSQL_PARAMETERS);
             this.driver = "com.mysql.jdbc.Driver";
             this.properties.put("driver", this.driver);
--- Common/src/main/java/com/conan/bigdata/common/databases/JDBC.java
+++ Common/src/main/java/com/conan/bigdata/common/databases/JDBC.java
@@ -1,8 +1,10 @@
 package com.conan.bigdata.common.databases;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
 
 /**
  * Created by Administrator on 2018/9/14.
@@ -10,16 +12,43 @@ import java.sql.SQLException;
 public class JDBC {
     private static Connection conn = null;
 
-    static {
+    public void getConnection(String className, String url, String user, String passwd) {
         try {
-            Class.forName(DB.TEST_MYSQL_19_6.driver);
-            conn = DriverManager.getConnection(DB.TEST_MYSQL_19_6.url, DB.TEST_MYSQL_19_6.user, DB.TEST_MYSQL_19_6.password);
+            Class.forName(className);
+            conn = DriverManager.getConnection(url, user, passwd);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public void show(ResultSet rs) throws SQLException {
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        while (rs.next()) {
+            // 读所有的schema和数据
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.println(metaData.getColumnName(i) + "\t" + metaData.getColumnTypeName(i) + "\t" + rs.getObject(i));
+            }
+
+            // 读DATE类型的数据
+            TimeZone timeZone = TimeZone.getTimeZone("GMT+8");
+            long time = rs.getDate("birthday").getTime();
+            System.out.println(rs.getString("birthday"));
+            System.out.println(time);
+            System.out.println(new Date(time));
+            String timeFormat = DateFormatUtils.format(new Date(time), "yyyy-MM-dd HH:mm:ss", timeZone);
+            System.out.println(timeFormat);
+        }
+    }
+
     public static void main(String[] args) throws SQLException {
+        JDBC jdbc = new JDBC();
+        jdbc.getConnection(DB.MYSQL_18_81_COUPONS.driver, DB.MYSQL_18_81_COUPONS.url, DB.MYSQL_18_81_COUPONS.user, DB.MYSQL_18_81_COUPONS.password);
+        System.out.println(conn);
+        // 000094195827   000000074053
+        PreparedStatement ps = conn.prepareStatement("select * from cp_card_member where m_shopid = 43 and card_no = '000094195827'");
+        ResultSet rs = ps.executeQuery();
+        jdbc.show(rs);
         System.out.println(conn.isClosed());
     }
 }
