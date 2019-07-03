e52055422c8175aeb70022ea94a156e00c5c93ae
liufeiqiang <liu.feiqiang@puscene.com>
2019-07-03 14:59:25 +0800

    update

---
+++ Hadoop/src/main/java/com/conan/bigdata/hadoop/mr/MultipleOutputDir.java
@@ -0,0 +1,88 @@
+package com.conan.bigdata.hadoop.mr;
+
+import com.conan.bigdata.hadoop.util.HadoopConf;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2019/7/3.
+ * <p>
+ * hadoop 任务输出到多个目录里面, 默认是一个输出目录
+ */
+public class MultipleOutputDir extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = getConf();
+        Job job = Job.getInstance(conf, "MultipleOutputDir");
+        Path out = new Path("/user/hive/temp/out");
+        FileSystem fs = FileSystem.get(conf);
+        if (fs.exists(out)) {
+            fs.delete(out, true);
+        }
+        job.setJarByClass(MultipleOutputDir.class);
+        job.setMapperClass(MultipleOutputDirMapper.class);
+        job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setNumReduceTasks(0);
+        job.setInputFormatClass(TextInputFormat.class);
+        // 去除输出目录下， 几个part- 开头的空文件
+        LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
+        // job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path("/user/hive/temp/in/"));
+        FileOutputFormat.setOutputPath(job, out);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int result = ToolRunner.run(HadoopConf.getHAInstance(), new MultipleOutputDir(), args);
+        System.exit(result);
+    }
+
+    public static class MultipleOutputDirMapper extends Mapper<LongWritable, Text, NullWritable, Text> {
+        private MultipleOutputs<NullWritable, Text> multipleOutputs = null;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            multipleOutputs = new MultipleOutputs<>(context);
+        }
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            // 这里有一个重载方法， 注意，两个方法功能一样，但是有区别
+            multipleOutputs.write(NullWritable.get(), value, getFileName(value));
+        }
+
+        private String getFileName(Text value) {
+            String country[] = value.toString().split("\\s+");
+            // 如果没有这个 / , 则文件名为 china-m-00000
+            return country[0].toLowerCase() + "/file";
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            // 一定要关闭这个，否则会没有输出
+            if (multipleOutputs != null)
+                multipleOutputs.close();
+        }
+    }
+
+}
