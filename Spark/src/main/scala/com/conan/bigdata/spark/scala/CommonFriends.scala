6a7c2d7ca94565ba091a63fbbcdd2ea35e6ad664
liufeiqiang <liu.feiqiang@puscene.com>
2019-07-03 18:18:24 +0800

    update

---
+++ Spark/src/main/scala/com/conan/bigdata/spark/scala/CommonFriends.scala
@@ -0,0 +1,22 @@
+package com.conan.bigdata.spark.scala
+
+/**
+  * 计算下面用户的共同好友， 冒号前是一个用户， 冒号后是该用户的好友
+  * A:B,C,D,F,E,O
+  * B:A,C,E,K
+  * C:F,A,D,I
+  * D:A,E,F,L
+  * E:B,C,D,M,L
+  * F:A,B,C,D,E,O,M
+  * G:A,C,D,E,F
+  * H:A,C,D,E,O
+  * I:A,O
+  * J:B,O
+  * K:A,C,D
+  * L:D,E,F
+  * M:E,F,G
+  * O:A,H,I,J
+  */
+object CommonFriends {
+
+}
