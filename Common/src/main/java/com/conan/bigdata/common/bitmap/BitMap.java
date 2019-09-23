671cfeca52323c834379a0a91ba979a3abccc516
liufeiqiang <liu.feiqiang@puscene.com>
2019-09-23 16:14:24 +0800

    update

--- Common/src/main/java/com/conan/bigdata/common/bitmap/BitMap.java
+++ Common/src/main/java/com/conan/bigdata/common/bitmap/BitMap.java
@@ -8,12 +8,22 @@ public class BitMap {
 
     public static void main(String[] args) {
         bitMap(10);
-        bitMap(33);
+//        bitMap(33);
         System.out.println(a[0]);
+
+        // 打印整数的二进制
+        System.out.println(Integer.toBinaryString(10000000));
+        System.out.println(Long.toBinaryString(5000000000L));
+
+        // 二进制转十进制
+        System.out.println(Integer.parseInt("100110001001011010000000",2));
+        System.out.println(Long.parseLong("100101010000001011111001000000000",2));
     }
 
     public static void bitMap(int n) {
         int row = n >> 5;
+        System.out.println("row = " + row);
+        System.out.println("n & 0x1F = " + (n & 0x1F));
         a[row] |= 1 << (n & 0x1F);
 
         // 移位的数字 移位符 移位次数
